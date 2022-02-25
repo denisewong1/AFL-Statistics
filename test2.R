a269afbae9d4b8708b7322a198330b6e8c633600
denisewong1 <40827382+denisewong1@users.noreply.github.com>
2022-02-26 07:26:15 +1100

    Update test2.R

--- test2.R
+++ test2.R
@@ -1,3 +1,2 @@
 print("sourcing test file")
-setwd("https://raw.github.com/denisewong1/AFL-Statistics/master/")
-source("test.R")
+source_url('https://raw.github.com/denisewong1/AFL-Statistics/master/test.R')
